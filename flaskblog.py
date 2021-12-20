947aa9b6e3821edde88250126f691253b6839acb
Mostafa-ashraf19 <mostaphaashraf1996@gmail.com>
2021-12-20 15:28:47 +0200

    Handle Login and Registration.
    Added login and registration pages, Improve styling, some bugs fixed.

--- flaskblog.py
+++ flaskblog.py
@@ -1,7 +1,12 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash
+from flask.helpers import flash
+from werkzeug.utils import redirect
+
+from forms import LoginForm, RegestrationForm
 
 app = Flask(__name__)
 
+app.config['SECRET_KEY'] = '8bee0a560054550f03c9d0eeed455e78'
 
 posts = [
     {
@@ -21,14 +26,38 @@ posts = [
 
 @app.route("/")
 @app.route("/home")
-def hello_world():
+def home():
     return render_template('home.html', posts=posts)
 
 
 @app.route("/about")
 def about():
-    return render_template('about.html',title='About')
+    return render_template('about.html', title='About')
 
 
-# if __name__ = '__main__':
-#     app.run(degbug=True)
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegestrationForm()
+
+    # This is a shortcut for form.is_submitted() and form.validate().
+    if form.validate_on_submit():
+        flash(f'Account created Done, User{form.username.data}', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+
+    if form.validate_on_submit():
+        if form.email.data == 'admin@flask.com' and form.password.data == '123456789':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login unsuccessful. Check your username and Password.', 'danger')
+    return render_template('login.html', title='Login', form=form)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ forms.py
@@ -0,0 +1,29 @@
+from flask_wtf import FlaskForm
+
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import Length, DataRequired, Email, EqualTo
+
+
+class RegestrationForm(FlaskForm):
+    username = StringField('Username', validators=[
+                           DataRequired(), Length(2, 20)])
+
+    email = StringField('Email', validators=[DataRequired(), Email()])
+
+    password = PasswordField('Password', validators=[DataRequired()])
+
+    confirm_password = PasswordField('Confirm Password', validators=[
+                                     DataRequired(), EqualTo('password')])
+
+    submit = SubmitField('Sign Up')
+
+
+class LoginForm(FlaskForm):
+
+    email = StringField('Email', validators=[DataRequired(), Email()])
+
+    password = PasswordField('Password', validators=[DataRequired()])
+
+    remember = BooleanField('Remember Me')
+
+    submit = SubmitField('Login')
