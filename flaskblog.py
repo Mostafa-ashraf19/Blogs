165c956633e9c0a468962b40bcf90ffb5d028d21
Mostafa-ashraf19 <mostaphaashraf1996@gmail.com>
2021-12-20 17:13:11 +0200

    Handled Databases with ORM SQLAlchemy
    Support Database with object relational mapper SQLAlchemy, Used SQLite for development and Postgess DB for production.

--- flaskblog.py
+++ flaskblog.py
@@ -2,11 +2,55 @@ from flask import Flask, render_template, url_for, flash
 from flask.helpers import flash
 from werkzeug.utils import redirect
 
+from datetime import datetime
+from flask_sqlalchemy import SQLAlchemy
+
 from forms import LoginForm, RegestrationForm
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = '8bee0a560054550f03c9d0eeed455e78'
+
+# BD Location.
+# Development Database.
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+
+# ORM, SQLite in development, Postgress in production.
+db = SQLAlchemy(app)
+
+# User to Post is one to many relations.
+
+
+class User(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False,
+                           default='default.jpg')
+    password = db.Column(db.String(60), nullable=False)
+
+    # Posts Ids.
+    posts = db.relationship('Post', backref='author', lazy=True)
+
+    # How object are printed.
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+
+
+class Post(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False,
+                            default=datetime.utcnow)
+    content = db.Column(db.Text, nullable=False)
+
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=True)
+
+    def __repr__(self):
+        return f"Post('{self.title}', '{self.date_posted}')"
+
 
 posts = [
     {
