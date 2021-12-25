2bd6b4264f6de23833ff75b3a393ab416cdf8000
Mostafa-ashraf19 <mostaphaashraf1996@gmail.com>
2021-12-25 15:01:57 +0200

    Added the delete post feature.

--- Flaskblog/routes.py
+++ Flaskblog/routes.py
@@ -147,3 +147,15 @@ def update_post(post_id):
         form.content.data = post.content
 
     return render_template('new_post.html', title='Update Post', legend='Update Post', form=form)
+
+
+@app.route('/post/<int:post_id>/delete', methods=['POST'])
+@login_required
+def delete_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    db.session.delete(post)
+    db.session.commit()
+    flash('Your post has been deleted successfully!', 'success')
+    return redirect(url_for('home'))
